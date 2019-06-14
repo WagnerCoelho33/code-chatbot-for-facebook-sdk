884c50b54a20cfda18e730101ace6242c07e07b2
WagnerCoelho33 <wagnermsc2014@gmail.com>
2019-06-14 09:02:02 -0300

    implementação da class para adicionar botão começar

---
+++ tests/integration/GetStartedButtonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CodeBot;
+
+
+use PHPUnit\Framework\TestCase;
+
+class GetStartedButtonTest extends TestCase
+{
+    public function testAddGetStartedButton()
+    {
+        $data = (new GetStartedButton())->add('iniciar');
+        $callSendApi = new CallSendApi('EAAFyZCCO5dmoBACbiDCZCRjc4ZBZCj6HzqW1rdT0WZBsdZCUHbfqsAQieCitQQLCwv9uOrsjU1ZAnRBADnOeZBzkjr7ve37P55A2sZBcsHvEJbKaG0g5BQBKb4FYx9FfypmYX0C9dzYZAQZAIeSiijVvNoHlMPFtUgUoggDFAUthWH5HgZDZD');
+        $result = $callSendApi->make($data, CallSendApi::URL_PROFILE);
+        
+        $this->assertTrue(is_string($result));
+    }
+}
